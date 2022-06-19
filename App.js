bfc320e25ca93e60832313b438d31dfe71e31288
Nora Khalid <nalobaidi_@hotmail.com>
2022-06-19 20:44:36 +0300

    create main profile uncomplete

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MainProfile from './MainProfile';
+import { NativeBaseProvider} from 'native-base';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider><View style={styles.container}>
+
+    <MainProfile/>
+    <StatusBar style="auto" />
+  </View>
+  </NativeBaseProvider>
+    
   );
 }
 
@@ -15,6 +20,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
 });
---
+++ mainProfileData.js
@@ -0,0 +1,8 @@
+ const profile =[{
+    username:"noraalobaidi_",
+    bio:"Always say yes to new adventures",
+    image:"https://a.cdn-hotels.com/gdcs/production18/d1838/041ae6b1-0a88-4c22-a648-53a22dd4a006.jpg?impolicy=fcrop&w=1600&h=1066&q=medium",
+    trips:["Santorini","Seoul","Florida"]
+}];
+
+export default profile;
